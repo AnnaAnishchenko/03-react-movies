0338cf7cc3fe9b5538f22fbe0d739cf82db1e5c4
Anna Anishchenko <annuwkaanishchenko@gmail.com>
2026-01-04 20:55:46 +0200

    add

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import "modern-normalize";
 import "./index.css";
-import App from "./App/App";
+import App from "./components/App/App";
 
 const rootElement = document.getElementById("root")!;
 
